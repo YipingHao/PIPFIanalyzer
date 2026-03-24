19ac184ca27aa0e00f10d0c4af1442d948f012c2
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2026-03-24 16:02:42 +0800

    bug fix %zu

--- code/parser.cpp
+++ code/parser.cpp
@@ -136,8 +136,8 @@ int build_raw(FILE*fp, vector<FIexpress> &exps, vector<size_t> &sites)
     if(error != 0)
     {
         //error_record00 = GrammarTree::ActionError;
-        fprintf(stderr, "head: %z\n", ASTree->error_record01);
-        fprintf(stderr, "top: %z\n", ASTree->error_record01);
+        fprintf(stderr, "head: %zu\n", ASTree->error_record01);
+        fprintf(stderr, "top: %zu\n", ASTree->error_record01);
         delete ASTree;
         return error;
     }
